bde0d6974587daf9ec008ef6d3064578ca602142
takehito koshimizu <takehito.koshimizu@gmail.com>
2019-02-11 17:45:49 +0900

    lightweight refactor

--- Sources/ColorListGen/main.swift
+++ Sources/ColorListGen/main.swift
@@ -5,8 +5,11 @@
 //  Created by Takehito Koshimizu on 2019/02/10.
 //
 
+#if canImport(ColorSetKit)
 import ColorSetKit
 
 if #available(macOS 10.12, *) {
-    ColorSets.ofCurrentDir().saveToUsersLibraryColors()
+    ColorSets.underCurrentDirectory().save()
 }
+
+#endif
