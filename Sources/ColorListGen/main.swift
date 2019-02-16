bb377f8d57faece7cc30e3dee91c04d0b1dc7252
takehito koshimizu <takehito.koshimizu@gmail.com>
2019-02-16 14:49:06 +0900

    add ShellKit

--- Sources/ColorListGen/main.swift
+++ Sources/ColorListGen/main.swift
@@ -5,11 +5,8 @@
 //  Created by Takehito Koshimizu on 2019/02/10.
 //
 
-#if canImport(ColorSetKit)
 import ColorSetKit
 
 if #available(macOS 10.12, *) {
-    ColorSets.currentDir().save()
+    ColorSets.currentDir().write()
 }
-
-#endif
